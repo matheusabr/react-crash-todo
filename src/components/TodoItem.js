12b9688640aa94b9b71b11cc39d1e827153829bd
Matheus de Oliveira Abreu <mabreu@pdmfc.com>
2019-01-17 14:57:56 +0000

    chore(): event trigger and action to checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,13 +5,33 @@ export class TodoItem extends Component {
   // Dynamic styling
   getStyle = () => {
     return {
+      background: '#f4f4f4',
+      padding: '10px',
+      border: '1px #ccc dashed',
       textDecoration: this.props.todo.isCompleted ? 'line-through' : 'none'
     }
   }
+
+  // Event
+  // Opt 1: you have to bind this instance
+  // markComplete(e) {
+  //   console.log(this.props);
+  // }
+  // Opt 2: arrow function
+  markComplete = (e) => {
+    console.log(this.props);
+  }
+
   render() {
     return (
       <div style={this.getStyle()}>
-        <p style={{ fontSize: 13, fontWeight: 'bold' }}>{this.props.todo.title}</p>
+        <p style={{ fontSize: 13, fontWeight: 'bold' }}>
+          {/* Opt 1: you have to bind this instance */}
+          {/* <input type="checkbox" onChange={this.markComplete.bind(this)} /> {' '} */}
+          {/* Opt 2: arrow function */}
+          <input type="checkbox" onChange={this.markComplete} /> {' '}
+          {this.props.todo.title}
+        </p>
       </div>
     )
   }
