9faf66f4e8cfdb84895c3679bf76e87a2df09a54
Matheus de Oliveira Abreu <mabreu@pdmfc.com>
2019-01-17 11:46:43 +0000

    chore(): create a Todos class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'; // Obs 01: or import React from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component { // Obs 01: class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>To Do List</h1>
+        <Todos />
       </div>
     );
   }
