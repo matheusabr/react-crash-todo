6ba1a3047a3010fadf7ac5fd5fa876730cd39cfc
Matheus de Oliveira Abreu <mabreu@pdmfc.com>
2019-01-17 12:13:22 +0000

    refactor(TodoItem): add new component and rende iterate its data

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import TodoItem from './TodoItem'
 
 class Todos extends Component {
   render() {
     // console.table(this.props.todos);
-    return this.props.todos.map(todo => (<h3>{todo.title}</h3>));
+    return this.props.todos.map(todo => (<TodoItem key={todo.id} todo={todo} />));
   }
 }
 
