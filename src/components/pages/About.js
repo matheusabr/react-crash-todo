cc40a9b6b19e3e0e3eff557e87b32d2cbffa1996
Matheus de Oliveira Abreu <mabreu@pdmfc.com>
2019-01-18 16:33:37 +0000

    style(About): create page layout

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    // React.Fragment act like a ghost to the DOM
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a To Do App</p>
+      <p>Made with ReactJS</p>
+      <p>Version 1.0.0</p>
+    </React.Fragment>
+  )
+}
+
+export default About
